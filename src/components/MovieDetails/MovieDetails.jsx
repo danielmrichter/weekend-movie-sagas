5909bff93874ca2af1d1ac9631120562cd1765c4
Daniel Richter <drichter3697@gmail.com>
2024-08-24 17:00:24 -0500

    implemented the desired mui elements

--- src/components/MovieDetails/MovieDetails.jsx
+++ src/components/MovieDetails/MovieDetails.jsx
@@ -1,25 +1,40 @@
 import { Dialog } from "primereact/dialog";
 import { useDispatch, useSelector } from "react-redux";
 import { Image } from "primereact/image";
+import { Box, Chip } from "@mui/material";
+
 export default function MovieDetails() {
   const dispatch = useDispatch();
   const visible = useSelector((store) => store.modalVisible);
   const currentMovie = useSelector((store) => store.currentMovie);
   return (
     <Dialog
-      header={<h2>{currentMovie.movieTitle}</h2>}
       visible={visible}
       style={{ width: "50vw", background: "white" }}
       onHide={() => dispatch({ type: "TOGGLE_VISIBILITY" })}
       closeOnEscape
+      focusOnShow
     >
-      <Image src={currentMovie.poster} alt="poster"></Image>
-      <div className="genre-list">
-        {currentMovie.movieGenre.map((genre) => (
-          <h4>{genre}&nbsp;</h4>
-        ))}
-      </div>
-      <p>{currentMovie.movieDescription}</p>
+      <Box
+        display="flex"
+        flexDirection="column"
+        justifyContent="center"
+        alignItems="center"
+      >
+        <h2>{currentMovie.movieTitle}</h2>
+        <Box display='flex'
+        justifyContent='space-evenly'
+        columnGap={5}
+        alignItems='center'>
+          <Image src={currentMovie.poster} alt="poster"></Image>
+          <Box display="flex" rowGap={2} flexDirection="column" alignItems="flex-end">
+            {currentMovie.movieGenre.map((genre) => (
+              <Chip label={genre}></Chip>
+            ))}
+          </Box>
+        </Box>
+        <p>{currentMovie.movieDescription}</p>
+      </Box>
     </Dialog>
   );
 }
